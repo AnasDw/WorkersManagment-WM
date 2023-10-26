1c5564178bc4417a612cc7044751c2fadb24968b
AnasDw <anas.e.dweik@gmail.com>
2023-10-26 19:36:10 +0300

    Design WorkPlace

--- src/components/WorkPlaceComponnents/WorkPlaceCard/Components/FeaturedDepCard.jsx
+++ src/components/WorkPlaceComponnents/WorkPlaceCard/Components/FeaturedDepCard.jsx
@@ -1,56 +1,61 @@
 import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions, Grid } from '@mui/material';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import EditNoteIcon from '@mui/icons-material/EditNote';
 
-import AspectRatio from '@mui/joy/AspectRatio';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import IconButton from '@mui/joy/IconButton';
-import Typography from '@mui/joy/Typography';
-import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
+// Imgs Imports
+import BG from '/Users/anasdweik/WorkersManagment-WM-1/src/assets/backgrounds/pexels-huseyn-kamaladdin-667838.jpg';
+import b1 from '/Users/anasdweik/WorkersManagment-WM-1/src/assets/backgrounds/b1.jpg';
+import b2 from '/Users/anasdweik/WorkersManagment-WM-1/src/assets/backgrounds/b2.jpg';
+import b3 from '/Users/anasdweik/WorkersManagment-WM-1/src/assets/backgrounds/b3.jpg';
+import b4 from '/Users/anasdweik/WorkersManagment-WM-1/src/assets/backgrounds/b4.jpg';
+import b5 from '/Users/anasdweik/WorkersManagment-WM-1/src/assets/backgrounds/b5.jpg';
 
-export default function FeaturedDepCard(props) {
+const ImgByIndex = (i) => {
+  if (i == 1) return b1;
+  if (i == 2) return b2;
+  if (i == 3) return b3;
+  if (i == 4) return b4;
+  if (i == 5) return b5;
+};
+
+export default function FeaturedDepCard({ props }) {
   return (
     <>
-      <Card sx={{ width: 320 }}>
-        <div>
-          <Typography level="title-lg">Yosemite National Park</Typography>
-          <Typography level="body-sm">April 24 to May 02, 2021</Typography>
-          <IconButton
-            aria-label="bookmark Bahamas Islands"
-            variant="plain"
-            color="neutral"
-            size="sm"
-            sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}
-          >
-            <BookmarkAdd />
-          </IconButton>
-        </div>
-        <AspectRatio minHeight="120px" maxHeight="200px">
-          <img
-            src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
-            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
-            loading="lazy"
-            alt=""
-          />
-        </AspectRatio>
-        <CardContent orientation="horizontal">
-          <div>
-            <Typography level="body-xs">Total price:</Typography>
-            <Typography fontSize="lg" fontWeight="lg">
-              $2,900
-            </Typography>
-          </div>
-          <Button
-            variant="solid"
-            size="md"
-            color="primary"
-            aria-label="Explore Bahamas Islands"
-            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
-          >
-            Explore
-          </Button>
-        </CardContent>
-      </Card>
+      <Grid container spacing={3} mt={8} justifyContent={'center'}>
+        {props.data.Positions.map((item, index) => (
+          <Grid key={index} item xs={12} sm={6} md={3}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia component="img" height="140" src={ImgByIndex(index + 1)} alt="green iguana" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {`${item.Dep} Department`}
+                  </Typography>
+                  <Grid mt={2} display={'flex'} container variant="body2" sx={{ gap: 1 }}>
+                    <EventNoteIcon />
+                    <Typography variant="body2" color="text.secondary">
+                      {`Contains ${item.Pos.length} Positions`}
+                    </Typography>
+                  </Grid>
+                </CardContent>
+              </CardActionArea>
+              <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small">
+                  <EditNoteIcon />
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
